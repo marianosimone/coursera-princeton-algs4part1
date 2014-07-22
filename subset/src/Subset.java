c7e9258f545d1db8c2e0e597a5d2c163e8b5b2b0
Mariano Simone <marianosimone@gmail.com>
2014-07-22 17:06:44 -0300

    add more specifications about subset

--- subset/src/Subset.java
+++ subset/src/Subset.java
@@ -1,4 +1,5 @@
 /**
+Subset client. Write a client program Subset.java that takes a command-line integer k; reads in a sequence of N strings from standard input using StdIn.readString(); and prints out exactly k of them, uniformly at random. Each item from the sequence can be printed out at most once. You may assume that k ≥ 0 and no greater than the number of string N on standard input. 
 The running time of Subset must be linear in the size of the input. You may use only a constant amount of memory
 plus either one Deque or RandomizedQueue object of maximum size at most N, where N is the number of strings on standard input.
 (For an extra challenge, use only one Deque or RandomizedQueue object of maximum size at most k.) 
